f4a448d54b5a8fe095ae023a61e25ccd3c382933
Asfandyar <a_asfandyar1@hotmail.com>
2024-04-05 03:56:37 +0100

    Initial Commit

---
+++ swift_day_0_of_100.playground/Contents.swift
@@ -0,0 +1,6 @@
+import Cocoa
+
+let temperature = 10.0
+let convert_to_fahrenheit = ((temperature*9)/5)+32
+
+print("temperature in celsius \(temperature) converted to fahrenheit \(convert_to_fahrenheit)°")
